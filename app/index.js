3888028c4139aa36a3e6a4a2ccdb9ee6bb4ebe0d
Sarthak Khandelwal <sarthakhandelwal@gmail.com>
2024-01-17 01:12:37 +0530

    project setup

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+// we can assume view as div and text as p or h tag
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+}
+
+export default Home;
