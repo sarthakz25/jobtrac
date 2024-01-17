e238c7094186bf56f3866424e8c3d074102541a2
Sarthak Khandelwal <sarthakhandelwal@gmail.com>
2024-01-18 03:00:15 +0530

    image url validation

---
+++ utils/index.js
@@ -0,0 +1,8 @@
+// to check if image url is correct
+export const checkImageUrl = (url) => {
+    if (!url) return false
+    else {
+        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
+        return pattern.test(url);
+    }
+};
