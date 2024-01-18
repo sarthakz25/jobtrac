b2d1eb0f4399a1997d307ec5eedcda35397df7fd
Sarthak Khandelwal <sarthakhandelwal@gmail.com>
2024-01-18 23:55:26 +0530

    implement job details

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    Linking
+} from "react-native";
 
 import styles from "./footer.style";
+import { icons } from "../../../constants";
 
-const Footer = () => {
+const Footer = ({ url }) => {
     return (
-        <View>
-            <Text>Footer</Text>
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.likeButton}>
+                <Image
+                    source={icons.heartOutline}
+                    resizeMode="contain"
+                    style={styles.likeButtonImage}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.applyButton}
+                onPress={() => Linking.openURL(url)}
+            >
+                <Text style={styles.applyButtonText}>Apply for job</Text>
+            </TouchableOpacity>
         </View>
     );
 }
